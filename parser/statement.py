8e6223d4f3d68acb7f6de1c8f9016745a71e2b2e
baderik <baderikpavel@gmail.com>
2023-11-12 19:23:16 +1000

    Delete unnecessary functions.

--- parser/statement.py
+++ parser/statement.py
@@ -15,37 +15,19 @@ class Statement:
         self._parse()
         self._calc_example_count()
 
-    def _parse_name(self):
-        with open(self.lang_path / "name.tex", encoding=self.encoding) as inp:
-            self.name = inp.read()
-
-    def _parse_legend(self):
-        with open(self.lang_path / "legend.tex", encoding=self.encoding) as inp:
-            self.legend = inp.read()
-
-    def _parse_input(self):
-        with open(self.lang_path / "input.tex", encoding=self.encoding) as inp:
-            self.input = inp.read()
-
-    def _parse_output(self):
-        with open(self.lang_path / "output.tex", encoding=self.encoding) as inp:
-            self.output = inp.read()
-
-    def _parse_notes(self):
-        with open(self.lang_path / "notes.tex", encoding=self.encoding) as inp:
-            self.notes = inp.read()
-
-    def _parse_tutorial(self):
-        with open(self.lang_path / "tutorial.tex", encoding=self.encoding) as inp:
-            self.tutorial = inp.read()
-
     def _parse(self):
-        self._parse_name()
-        self._parse_legend()
-        self._parse_input()
-        self._parse_output()
-        self._parse_notes()
-        self._parse_tutorial()
+        self.name = self._parse_file("name.tex")
+        self.legend = self._parse_file("legend.tex")
+        self.input = self._parse_file("input.tex")
+        self.output = self._parse_file("output.tex")
+        self.notes = self._parse_file("notes.tex")
+        self.tutorial = self._parse_file("notes.tex")
+
+    def _parse_file(self, name: str) -> str:
+        f_name = self.lang_path / name
+        if f_name.exists():
+            with open(f_name, encoding=self.encoding) as inp:
+                return inp.read()
 
     def _calc_example_count(self):
         self.count = 0
@@ -53,3 +35,14 @@ class Statement:
             if f.name not in _service_files:
                 self.count += 1
         self.count //= 2
+
+
+if __name__ == '__main__':
+    s = Statement(Path("../polygon/example-a-plus-b-11/statement-sections/english/"), "UTF-8")
+    print(s.legend)
+    print(s.input)
+    print(s.output)
+    print(s.count)
+    print(s.lang_path)
+    print(s.notes)
+    print(s.tutorial)
